cb7393f0193d5d6eb67bbde8f176f421b9f25f2d
Antasjain <antasjain2004@gmail.com>
2024-04-23 19:43:28 +0530

    Added some more responsivity

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -11,7 +11,7 @@ function LogoutBtn() {
     }
   return (
     <button
-    className='inline-bock px-4 ml-2 py-2  duration-200 hover:bg-blue-100 hover:text-black rounded-full text-red-600'
+    className='inline-bock px-4 ml-4 py-2  duration-200 hover:bg-blue-100 hover:text-black rounded-full text-red-600'
     onClick={logoutHandler}
     >Logout</button>
   )
